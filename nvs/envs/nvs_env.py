615534679b8411309a053a5ea27a9e3d284315aa
George Tan <georget1@andrew.cmu.edu>
2017-04-03 05:49:19 -0400

    Add comments to nvs env

--- nvs/envs/nvs_env.py
+++ nvs/envs/nvs_env.py
@@ -12,35 +12,42 @@ from nvs.envs.env_constants import data_folder, data_dict_file_name
 class NVSEnv():
 	metadata = {'render.modes': ['human']}
 
+	# Path to data and images
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	data_folder = os.path.join(dir_path, 'data')
 	image_folder = os.path.join(dir_path, 'modelnet40v1')
 
+	# Env state
 	category = None
 	group = None
 	image_idx = None
 	group_size = None
 	image = None
+
+	# Time delay during render
 	render_delay = 1
 
 	def __init__(self):
+		# Get dictionary of data
 		self.data = self.read_data()
 
+		# Get actions and count nS from data
 		self.actions = {}
 		self.nS = 0
-
 		for category in self.data['train']:
 			for group in self.data['train'][category]:
 				self.nS = self.nS + self.data['train'][category][group]['size']
 				label = self.data['train'][category][group]['label']
 				self.actions[label] = category
 
-		self.actions[len(self.actions)] = 'CW' # clockwise
-		self.actions[len(self.actions)] = 'CCW' # counter clockwise
+		# Add turn clockwise and turn counter clockwise actions
+		self.actions[len(self.actions)] = 'CW'
+		self.actions[len(self.actions)] = 'CCW'
 
 		self.nA = len(self.actions)
 
 	def reset(self):
+		# Get a random image and store the state
 		categories = list(self.data['train'].keys())
 		category = random.choice(categories)
 
@@ -59,17 +66,23 @@ class NVSEnv():
 		return self.image
 
 	def step(self, action):
-		if self.actions[action] == 'CCW':
-			self.image_idx = (self.image_idx - 1) % self.group_size
-			self.image = self.get_current_image()
-			obs, reward, is_terminal, info = self.image, 0, 0, {}
-		elif self.actions[action] == 'CW':
+		if self.actions[action] == 'CW':
+			# Increment image index by 1 to turn clockwise
+			# Ex: airplane_0627_001.jpg -> airplane_0627_002.jpg
 			self.image_idx = (self.image_idx + 1) % self.group_size
 			self.image = self.get_current_image()
 			obs, reward, is_terminal, info = self.image, 0, 0, {}
+		elif self.actions[action] == 'CCW':
+			# Decrement image index by 1 to turn counter clockwise
+			# Ex: airplane_0627_002.jpg -> airplane_0627_001.jpg
+			self.image_idx = (self.image_idx - 1) % self.group_size
+			self.image = self.get_current_image()
+			obs, reward, is_terminal, info = self.image, 0, 0, {}
 		elif self.actions[action] == self.category:
+			# Classify correctly
 			obs, reward, is_terminal, info = self.image, 1, 1, {}
 		else:
+			# Classify incorrectly
 			obs, reward, is_terminal, info = self.image, 0, 0, {}
 
 		return obs, reward, is_terminal, info
@@ -87,12 +100,14 @@ class NVSEnv():
 		np.random.seed(seed)
 
 	def read_data(self):
+		# Read dictionary of data from file created by format_data.py
 		full_data_dict_file_name = os.path.join(self.data_folder, data_dict_file_name)
 		data = [line for line in open(full_data_dict_file_name, 'r')][0]
 		data = ast.literal_eval(data)
 		return data
 
 	def get_current_image(self):
+		# Get RBG image 
 		image_path = self.data['train'][self.category][self.group]['images'][self.image_idx]
 		image = np.array(Image.open(image_path))
 		return image
