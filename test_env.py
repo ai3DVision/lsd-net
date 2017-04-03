af63c4605563db27205b9b052a85212e0bbaafb9
George Tan <georget1@andrew.cmu.edu>
2017-04-03 05:49:34 -0400

    Add test code for nvs env

---
+++ test_env.py
@@ -0,0 +1,37 @@
+from nvs.envs import NVSEnv
+
+env = NVSEnv()
+obs = env.reset()
+
+# Test moving the image closewise
+cw_idx = list(env.actions.values()).index('CW')
+cw_action = list(env.actions.keys())[cw_idx]
+for i in range(10):
+	obs, reward, is_terminal, info = env.step(cw_action)
+	env.render()
+
+# Change the render delay time faster
+env.set_render_delay(0.5)
+
+# Test moving the image counter closewise
+ccw_idx = list(env.actions.values()).index('CCW')
+ccw_action = list(env.actions.keys())[ccw_idx]
+for i in range(10):
+	obs, reward, is_terminal, info = env.step(ccw_action)
+	env.render()
+
+# Test guessing incorrectly
+category = env.category
+category_idx = list(env.actions.values()).index(category) + 1
+category_action = list(env.actions.keys())[category_idx]
+obs, reward, is_terminal, info = env.step(category_action)
+assert(reward == 0)
+assert(is_terminal == 0)
+
+# Test guessing correctly
+category = env.category
+category_idx = list(env.actions.values()).index(category)
+category_action = list(env.actions.keys())[category_idx]
+obs, reward, is_terminal, info = env.step(category_action)
+assert(reward == 1)
+assert(is_terminal == 1)
