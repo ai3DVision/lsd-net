c7a193ff666f9e7f9007efd5d15b87aa879bbe59
George Tan <georget1@andrew.cmu.edu>
2017-04-10 05:41:37 -0400

    Add script for NBV with GA3C

---
+++ ga3c_nbv.py
@@ -0,0 +1,42 @@
+# check python version; warn if not Python3
+import sys
+import warnings
+if sys.version_info < (3,0):
+    warnings.warn("Optimized for Python3. Performance may suffer under Python2.", Warning)
+
+import gym
+
+from GA3C.ga3c.Config import Config
+from GA3C.ga3c.Server import Server
+from GA3C.ga3c.env.CartPoleEnvironment import CartPoleEnvironment
+
+# Parse arguments
+for i in range(1, len(sys.argv)):
+    # Config arguments should be in format of Config=Value
+    # For setting booleans to False use Config=
+    x, y = sys.argv[i].split('=')
+    setattr(Config, x, type(getattr(Config, x))(y))
+
+Config.NETWORK_NAME = 'nbv'
+Config.GAME = 'Next-Best-View-v0'
+Config.PLAY_MODE = False
+Config.AGENTS = 4
+# Config.PREDICTORS = 1
+# Config.TRAINERS = 1
+# Config.DYNAMIC_SETTINGS = False
+
+Config.STACKED_FRAMES = 3
+Config.IMAGE_WIDTH = 224
+Config.IMAGE_HEIGHT = 224
+
+Config.GREEDY_POLICY = False
+Config.TMAX = 4
+Config.EPISODES = 50000
+
+Config.TENSORBOARD = True
+Config.TENSORBOARD_UPDATE_FREQUENCY = 100
+
+gym.undo_logger_setup()
+
+# Start main program
+Server().main()
