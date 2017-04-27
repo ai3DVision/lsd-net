c8f4aff4fad5f7f2cfebb1730f434bc6bbca5329
George Tan <georget1@andrew.cmu.edu>
2017-04-27 17:29:37 -0400

    Change back to 32 agents

--- GA3C/ga3c/Config.py
+++ GA3C/ga3c/Config.py
@@ -46,7 +46,7 @@ class Config:
     
     # If the dynamic configuration is on, these are the initial values.
     # Number of Agents
-    AGENTS = 8
+    AGENTS = 32 
     # Number of Predictors
     PREDICTORS = 2
     # Number of Trainers
